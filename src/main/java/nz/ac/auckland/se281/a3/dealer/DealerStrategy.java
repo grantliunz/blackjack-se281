b40406ee4c9414ab9d94f3bdddc6c37b75a527fb
Grant <gliu871@aucklanduni.ac.nz>
2022-05-02 20:49:49 +1200

    created initial inteface and classes for dealer strategies

---
+++ src/main/java/nz/ac/auckland/se281/a3/dealer/DealerStrategy.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se281.a3.dealer;
+
+import nz.ac.auckland.se281.a3.Participant.Action;
+
+public interface DealerStrategy {
+	Action action(int score);
+
+
+}
---
+++ src/main/java/nz/ac/auckland/se281/a3/dealer/TargetTopWinner.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281.a3.dealer;
+
+import nz.ac.auckland.se281.a3.Participant.Action;
+
+public class TargetTopWinner implements DealerStrategy {
+
+	@Override
+	public Action action(int score) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
